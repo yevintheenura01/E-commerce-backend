92906c3f8f4ea2ef4e36bd7565c2046742763a2c
Suresh0314 <it21433788@my.sliit.lk>
2026-03-28 13:05:25 +0530

    add servers

---
+++ server.js
@@ -0,0 +1,21 @@
+const dotenv = require("dotenv");
+const app = require("./app");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
